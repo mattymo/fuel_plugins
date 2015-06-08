189f20f2541e8cfc280fb5273595e2e01c994a68
Matthew Mosesohn <mmosesohn@mirantis.com>
2015-06-08 14:34:47 +0300

    Add todos for cleaner install

--- gamma-lcp-ui/deployment_scripts/install.sh
+++ gamma-lcp-ui/deployment_scripts/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+#TODO: move yamls to deployment_scripts/roles/ and read all yamls in dir
 declare -a roles=(gamma-lcp-dbng gamma-lcp-nova)
 
 DEBUG=true
 DIR=`dirname ${BASH_SOURCE[0]}`
 FUEL='/usr/bin/fuel'
+#FIXME: rels are dynamic and will be different on upgraded Fuel node
 #Releases:
 #CENTOS=1
 #UBUNTU=2
@@ -17,7 +19,10 @@ function debug {
 }
 
 function create_roles {
+  #FIXME: parse rels from fuel rel (and optionally grep -i ubuntu)
+  #This will break if you try to apply to an upgraded env
   for role in ${roles[@]}; do
+     #FIXME: if/else is safer in rpm script because of -e bash mode
      $FUEL role --rel $REL | grep -q $role && \
      $FUEL role --rel $REL --update --file ${DIR}/${role}.yaml || \
      $FUEL role --rel $REL --create --file ${DIR}/${role}.yaml
@@ -26,6 +31,8 @@ function create_roles {
 }
 
 create_roles
+#FIXME: needs to to /etc/puppet/$FUEL_REL/modules because /etc/pup/mod is symlink
 cp -a ${DIR}/gamma-lcp /etc/puppet/modules/osnailyfacter/modular/
+#FIXME: just sync tasks for current release
 $FUEL  rel --sync-deployment-tasks --dir /etc/puppet/
 
